adb1d4520c757e5bb6f25e79946ee18fce25620d
Stephen mwendwa <mwendwastephen877@gmail.com>
2024-11-06 13:10:08 +0300

    Add files via upload

---
+++ CAT_2_. question .cpp
@@ -0,0 +1,33 @@
+//program for c structures 
+/*
+//program for c structures
+Author: Stephen mwendwa 
+Date:20/10/2024
+*/
+#include<stdio.h>
+#include<string.h>
+   struct structure {
+   char Name[25];
+   char Department [20];
+   char Email[50];
+   float salary; 
+   int ID;
+ } employee1;
+ 
+ int main () {
+ strcpy(employee1.Department,"Human resources ");
+ strcpy(employee1.Name,"John Doe");
+ strcpy(employee1.Email,"John.doe@company.com");
+ employee1.salary=55000.50;
+ employee1.ID=12345;
+ 
+ 
+ printf("Name:%s\n", employee1.Name);
+ printf("Department:%s\n", employee1.Department);
+ printf("Email:%s\n", employee1.Email);
+ printf("salary:%f\n", employee1.salary);
+ printf("ID:%d\n", employee1.ID);
+ 
+ return 0;
+ 
+ }
